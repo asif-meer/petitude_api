d57d53c9540b1872f0ac6e94c8f8e02bcac002f3
one <one@test.com>
2015-10-01 16:44:08 +0500

    testing services

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  acts_as_token_authentication_handler_for User
+  # acts_as_token_authentication_handler_for User
 
     # Security note: controllers with no-CSRF protection must disable the Devise fallback,
     # see #49 for details.
